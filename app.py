d57e88910d0ca3452fbe4a811bea3366f4a1ca44
Diazonic Labs <61781809+diazoniclabs@users.noreply.github.com>
2023-08-10 16:26:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import joblib
+st.title("Underweight / Normal / Overweight Predictor")
+weight = st.slider('What is your weight?', 0, 100, 25)
+height = st.slider('What is your height?', 0, 200, 25)
+model = joblib.load('model_new')
+result = model.predict([[weight,height]])[0]
+
+if st.button("Predict"):
+  st.write(f"You are {result}")
